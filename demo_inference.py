46aa51dd65c41c248a98db3576a9fcddea0643f6
athrunsunny <43680123+athrunsunny@users.noreply.github.com>
2024-06-17 15:39:07 +0800

    Update demo_inference.py

--- demo_inference.py
+++ demo_inference.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 from PIL import Image
 import requests
@@ -10,15 +11,260 @@ from skimage.measure import find_contours
 
 from matplotlib import patches, lines
 from matplotlib.patches import Polygon
+from models import build_model
 
 torch.set_grad_enabled(False)
 
-# standard PyTorch mean-std input image normalization
-transform = T.Compose([
-    T.Resize(800),
-    T.ToTensor(),
-    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser("Set transformer detector", add_help=False)
+    parser.add_argument("--run_name", default="", type=str)
+
+    # Dataset specific
+    # parser.add_argument("--dataset_config", default=None, required=True)
+    parser.add_argument("--do_qa", action="store_true", help="Whether to do question answering")
+    parser.add_argument(
+        "--predict_final",
+        action="store_true",
+        help="If true, will predict if a given box is in the actual referred set. Useful for CLEVR-Ref+ only currently.",
+    )
+    parser.add_argument("--no_detection", action="store_true", help="Whether to train the detector")
+    parser.add_argument(
+        "--split_qa_heads", action="store_true", help="Whether to use a separate head per question type in vqa"
+    )
+    parser.add_argument(
+        "--combine_datasets", nargs="+", help="List of datasets to combine for training", default=["flickr"]
+    )
+    parser.add_argument(
+        "--combine_datasets_val", nargs="+", help="List of datasets to combine for eval", default=["flickr"]
+    )
+
+    parser.add_argument("--coco_path", type=str, default="")
+    parser.add_argument("--vg_img_path", type=str, default="")
+    parser.add_argument("--vg_ann_path", type=str, default="")
+    parser.add_argument("--clevr_img_path", type=str, default="")
+    parser.add_argument("--clevr_ann_path", type=str, default="")
+    parser.add_argument("--phrasecut_ann_path", type=str, default="")
+    parser.add_argument(
+        "--phrasecut_orig_ann_path",
+        type=str,
+        default="",
+    )
+    parser.add_argument("--modulated_lvis_ann_path", type=str, default="")
+
+    # Training hyper-parameters
+    parser.add_argument("--lr", default=1e-4, type=float)
+    parser.add_argument("--lr_backbone", default=1e-5, type=float)
+    parser.add_argument("--text_encoder_lr", default=5e-5, type=float)
+    parser.add_argument("--batch_size", default=2, type=int)
+    parser.add_argument("--weight_decay", default=1e-4, type=float)
+    parser.add_argument("--epochs", default=40, type=int)
+    parser.add_argument("--lr_drop", default=35, type=int)
+    parser.add_argument(
+        "--epoch_chunks",
+        default=-1,
+        type=int,
+        help="If greater than 0, will split the training set into chunks and validate/checkpoint after each chunk",
+    )
+    parser.add_argument("--optimizer", default="adam", type=str)
+    parser.add_argument("--clip_max_norm", default=0.1, type=float, help="gradient clipping max norm")
+    parser.add_argument(
+        "--eval_skip",
+        default=1,
+        type=int,
+        help='do evaluation every "eval_skip" frames',
+    )
+
+    parser.add_argument(
+        "--schedule",
+        default="linear_with_warmup",
+        type=str,
+        choices=("step", "multistep", "linear_with_warmup", "all_linear_with_warmup"),
+    )
+    parser.add_argument("--ema", action="store_true")
+    parser.add_argument("--ema_decay", type=float, default=0.9998)
+    parser.add_argument("--fraction_warmup_steps", default=0.01, type=float, help="Fraction of total number of steps")
+
+    # Model parameters
+    parser.add_argument(
+        "--frozen_weights",
+        type=str,
+        default=None,
+        help="Path to the pretrained model. If set, only the mask head will be trained",
+    )
+
+    parser.add_argument(
+        "--freeze_text_encoder", action="store_true", help="Whether to freeze the weights of the text encoder"
+    )
+
+    parser.add_argument(
+        "--text_encoder_type",
+        default="roberta-base",
+        choices=("roberta-base", "distilroberta-base", "roberta-large"),
+    )
+
+    # Backbone
+    parser.add_argument(
+        "--backbone",
+        default="resnet101",
+        type=str,
+        help="Name of the convolutional backbone to use such as resnet50 resnet101 timm_tf_efficientnet_b3_ns",
+    )
+    parser.add_argument(
+        "--dilation",
+        action="store_true",
+        help="If true, we replace stride with dilation in the last convolutional block (DC5)",
+    )
+    parser.add_argument(
+        "--position_embedding",
+        default="sine",
+        type=str,
+        choices=("sine", "learned"),
+        help="Type of positional embedding to use on top of the image features",
+    )
+
+    # Transformer
+    parser.add_argument(
+        "--enc_layers",
+        default=6,
+        type=int,
+        help="Number of encoding layers in the transformer",
+    )
+    parser.add_argument(
+        "--dec_layers",
+        default=6,
+        type=int,
+        help="Number of decoding layers in the transformer",
+    )
+    parser.add_argument(
+        "--dim_feedforward",
+        default=2048,
+        type=int,
+        help="Intermediate size of the feedforward layers in the transformer blocks",
+    )
+    parser.add_argument(
+        "--hidden_dim",
+        default=256,
+        type=int,
+        help="Size of the embeddings (dimension of the transformer)",
+    )
+    parser.add_argument("--dropout", default=0.1, type=float, help="Dropout applied in the transformer")
+    parser.add_argument(
+        "--nheads",
+        default=8,
+        type=int,
+        help="Number of attention heads inside the transformer's attentions",
+    )
+    parser.add_argument("--num_queries", default=100, type=int, help="Number of query slots")
+    parser.add_argument("--pre_norm", action="store_true")
+    parser.add_argument(
+        "--no_pass_pos_and_query",
+        dest="pass_pos_and_query",
+        action="store_false",
+        help="Disables passing the positional encodings to each attention layers",
+    )
+
+    # Segmentation
+    parser.add_argument(
+        "--mask_model",
+        default="none",
+        type=str,
+        choices=("none", "smallconv", "v2"),
+        help="Segmentation head to be used (if None, segmentation will not be trained)",
+    )
+    parser.add_argument("--remove_difficult", action="store_true")
+    parser.add_argument("--masks", action="store_true")
+
+    # Loss
+    parser.add_argument(
+        "--no_aux_loss",
+        dest="aux_loss",
+        action="store_false",
+        help="Disables auxiliary decoding losses (loss at each layer)",
+    )
+    parser.add_argument(
+        "--set_loss",
+        default="hungarian",
+        type=str,
+        choices=("sequential", "hungarian", "lexicographical"),
+        help="Type of matching to perform in the loss",
+    )
+
+    parser.add_argument("--contrastive_loss", action="store_true", help="Whether to add contrastive loss")
+    parser.add_argument(
+        "--no_contrastive_align_loss",
+        dest="contrastive_align_loss",
+        action="store_false",
+        help="Whether to add contrastive alignment loss",
+    )
+
+    parser.add_argument(
+        "--contrastive_loss_hdim",
+        type=int,
+        default=64,
+        help="Projection head output size before computing normalized temperature-scaled cross entropy loss",
+    )
+
+    parser.add_argument(
+        "--temperature_NCE", type=float, default=0.07, help="Temperature in the  temperature-scaled cross entropy loss"
+    )
+
+    # * Matcher
+    parser.add_argument(
+        "--set_cost_class",
+        default=1,
+        type=float,
+        help="Class coefficient in the matching cost",
+    )
+    parser.add_argument(
+        "--set_cost_bbox",
+        default=5,
+        type=float,
+        help="L1 box coefficient in the matching cost",
+    )
+    parser.add_argument(
+        "--set_cost_giou",
+        default=2,
+        type=float,
+        help="giou box coefficient in the matching cost",
+    )
+    # Loss coefficients
+    parser.add_argument("--ce_loss_coef", default=1, type=float)
+    parser.add_argument("--mask_loss_coef", default=1, type=float)
+    parser.add_argument("--dice_loss_coef", default=1, type=float)
+    parser.add_argument("--bbox_loss_coef", default=5, type=float)
+    parser.add_argument("--giou_loss_coef", default=2, type=float)
+    parser.add_argument("--qa_loss_coef", default=1, type=float)
+    parser.add_argument(
+        "--eos_coef",
+        default=0.1,
+        type=float,
+        help="Relative classification weight of the no-object class",
+    )
+    parser.add_argument("--contrastive_loss_coef", default=0.1, type=float)
+    parser.add_argument("--contrastive_align_loss_coef", default=1, type=float)
+
+    # Run specific
+
+    parser.add_argument("--test", action="store_true", help="Whether to run evaluation on val or test set")
+    parser.add_argument("--test_type", type=str, default="test", choices=("testA", "testB", "test"))
+    parser.add_argument("--output-dir", default="", help="path where to save, empty for no saving")
+    parser.add_argument("--device", default="cuda", help="device to use for training / testing")
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
+    parser.add_argument("--load", default="", help="resume from checkpoint")
+    parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="start epoch")
+    parser.add_argument("--eval", action="store_true", help="Only run evaluation")
+    parser.add_argument("--num_workers", default=5, type=int)
+
+    # Distributed training parameters
+    parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
+    parser.add_argument("--dist-url", default="env://", help="url used to set up distributed training")
+    return parser
 
 
 # for output bounding box post-processing
@@ -34,11 +280,6 @@ def rescale_bboxes(out_bbox, size):
     b = box_cxcywh_to_xyxy(out_bbox)
     b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
     return b
-
-
-# colors for visualization
-COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
-          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
 
 
 def apply_mask(image, mask, color, alpha=0.5):
@@ -84,50 +325,20 @@ def plot_results(pil_img, scores, boxes, labels, masks=None):
     plt.show()
 
 
-def add_res(results, ax, color='green'):
-    # for tt in results.values():
-    if True:
-        bboxes = results['boxes']
-        labels = results['labels']
-        scores = results['scores']
-        # keep = scores >= 0.0
-        # bboxes = bboxes[keep].tolist()
-        # labels = labels[keep].tolist()
-        # scores = scores[keep].tolist()
-    # print(torchvision.ops.box_iou(tt['boxes'].cpu().detach(), torch.as_tensor([[xmin, ymin, xmax, ymax]])))
+def plot_inference(im, caption):
+    # standard PyTorch mean-std input image normalization
+    transform = T.Compose([
+        T.Resize(800),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-    colors = ['purple', 'yellow', 'red', 'green', 'orange', 'pink']
-
-    for i, (b, ll, ss) in enumerate(zip(bboxes, labels, scores)):
-        ax.add_patch(plt.Rectangle((b[0], b[1]), b[2] - b[0], b[3] - b[1], fill=False, color=colors[i], linewidth=3))
-        cls_name = ll if isinstance(ll, str) else CLASSES[ll]
-        text = f'{cls_name}: {ss:.2f}'
-        print(text)
-        ax.text(b[0], b[1], text, fontsize=15, bbox=dict(facecolor='white', alpha=0.8))
-
-
-model_qa = torch.hub.load('ashkamath/mdetr:main', 'mdetr_efficientnetB5_gqa', pretrained=True,
-                          return_postprocessor=False)
-model_qa = model_qa.cuda()
-model_qa.eval()
-
-import json
-
-answer2id_by_type = json.load(
-    requests.get("https://nyu.box.com/shared/static/j4rnpo8ixn6v0iznno2pim6ffj3jyaj8.json", stream=True).raw)
-id2answerbytype = {}
-for ans_type in answer2id_by_type.keys():
-    curr_reversed_dict = {v: k for k, v in answer2id_by_type[ans_type].items()}
-    id2answerbytype[ans_type] = curr_reversed_dict
-
-
-def plot_inference_qa(im, caption):
     # mean-std normalize the input image (batch-size: 1)
     img = transform(im).unsqueeze(0).cuda()
 
     # propagate through the model
-    memory_cache = model_qa(img, [caption], encode_and_save=True)
-    outputs = model_qa(img, [caption], encode_and_save=False, memory_cache=memory_cache)
+    memory_cache = model(img, [caption], encode_and_save=True)
+    outputs = model(img, [caption], encode_and_save=False, memory_cache=memory_cache)
 
     # keep only predictions with 0.7+ confidence
     probas = 1 - outputs['pred_logits'].softmax(-1)[0, :, -1].cpu()
@@ -148,19 +359,24 @@ def plot_inference_qa(im, caption):
     labels = [predicted_spans[k] for k in sorted(list(predicted_spans.keys()))]
     plot_results(im, probas[keep], bboxes_scaled, labels)
 
-    # Classify the question type
-    type_conf, type_pred = outputs["pred_answer_type"].softmax(-1).max(-1)
-    ans_type = type_pred.item()
-    types = ["obj", "attr", "rel", "global", "cat"]
 
-    ans_conf, ans = outputs[f"pred_answer_{types[ans_type]}"][0].softmax(-1).max(-1)
-    answer = id2answerbytype[f"answer_{types[ans_type]}"][ans.item()]
-    print(f"Predicted answer: {answer}\t confidence={round(100 * type_conf.item() * ans_conf.item(), 2)}")
-    url = "https://s3.us-east-1.amazonaws.com/images.cocodataset.org/val2017/000000076547.jpg"
-    im3 = Image.open(requests.get(url, stream=True).raw)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("DETR training and evaluation script", parents=[get_args_parser()])
+    args = parser.parse_args()
+    model, criterion, contrastive_criterion, qa_criterion, weight_dict = build_model(args)
 
+    model_path = r'F:\project\mdetr\pretrained_resnet101_checkpoint.pth'
+    state_dict = torch.load(model_path, map_location='cpu')
+    model.load_state_dict(state_dict['model'], False)
 
-url = "https://s3.us-east-1.amazonaws.com/images.cocodataset.org/val2017/000000076547.jpg"
-im3 = Image.open(requests.get(url, stream=True).raw)
+    model.cuda()
+    model.eval()
 
-plot_inference_qa(im3, "What color is the train?")
+    url = "https://www.nextstagedesign.com/wp-content/uploads/2015/03/energy-efficient-appliances.jpg"
+    im = Image.open(requests.get(url, stream=True).raw)
+
+    # plot_inference(im, "all objects")
+    # plot_inference(im, "energy efficient")
+    plot_inference(im, "blue vase")
+    # plot_inference(im, "sink")
+    # plot_inference(im, "washing machine")
